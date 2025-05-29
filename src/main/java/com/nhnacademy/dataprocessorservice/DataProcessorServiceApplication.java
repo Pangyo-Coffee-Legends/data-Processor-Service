bececc6c28b31aceb180a70652e7f647b3666b81
inho <dusen0528@naver.com>
2025-05-29 14:05:26 +0900

    chore : spring.factories 추가

--- src/main/java/com/nhnacademy/dataprocessorservice/DataProcessorServiceApplication.java
+++ src/main/java/com/nhnacademy/dataprocessorservice/DataProcessorServiceApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableScheduling
 @EnableDiscoveryClient
-@EnableAspectJAutoProxy(exposeProxy = true)
+@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 public class DataProcessorServiceApplication {
 
     public static void main(String[] args) {
