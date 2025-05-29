07979bcebeb027f2310f542ceb45a9c566ea535a
inho <dusen0528@naver.com>
2025-05-29 11:34:17 +0900

    fix Mqtt 로깅 콜백 단계에서 MDC 설정하도록 변경

--- src/main/java/com/nhnacademy/dataprocessorservice/DataProcessorServiceApplication.java
+++ src/main/java/com/nhnacademy/dataprocessorservice/DataProcessorServiceApplication.java
@@ -2,12 +2,14 @@ package com.nhnacademy.dataprocessorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableDiscoveryClient
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class DataProcessorServiceApplication {
 
     public static void main(String[] args) {
